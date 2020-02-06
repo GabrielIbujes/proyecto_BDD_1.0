9e324a4387f8c19e85ddc67a874003440594a574
JESSICA <jessica.amoguimba@epn.edu.ec>
2020-02-06 10:44:28 -0500

    cambios

---
+++ proyecto1.0/proyecto1.0/Formulario103.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyecto1._0
+{
+    public partial class Formulario103 : Form
+    {
+        public Formulario103()
+        {
+            InitializeComponent();
+        }
+    }
+}
