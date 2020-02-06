729caaa2801d37f8382f75b69a25dba596589caf
JESSICA <jessica.amoguimba@epn.edu.ec>
2020-02-06 11:12:47 -0500

    ghj

---
+++ proyecto1.0/proyecto1.0/MenuPrincipal.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace proyecto1._0
+{
+    partial class MenuPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFormulario102A = new System.Windows.Forms.Button();
+            this.btnFormulario106 = new System.Windows.Forms.Button();
+            this.btnFormulario104 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFormulario102A
+            // 
+            this.btnFormulario102A.Location = new System.Drawing.Point(73, 111);
+            this.btnFormulario102A.Name = "btnFormulario102A";
+            this.btnFormulario102A.Size = new System.Drawing.Size(182, 23);
+            this.btnFormulario102A.TabIndex = 0;
+            this.btnFormulario102A.Text = "FORMULARIO 102A";
+            this.btnFormulario102A.UseVisualStyleBackColor = true;
+            // 
+            // btnFormulario106
+            // 
+            this.btnFormulario106.Location = new System.Drawing.Point(547, 111);
+            this.btnFormulario106.Name = "btnFormulario106";
+            this.btnFormulario106.Size = new System.Drawing.Size(168, 23);
+            this.btnFormulario106.TabIndex = 1;
+            this.btnFormulario106.Text = "FORMULARIO 106";
+            this.btnFormulario106.UseVisualStyleBackColor = true;
+            // 
+            // btnFormulario104
+            // 
+            this.btnFormulario104.Location = new System.Drawing.Point(320, 111);
+            this.btnFormulario104.Name = "btnFormulario104";
+            this.btnFormulario104.Size = new System.Drawing.Size(168, 23);
+            this.btnFormulario104.TabIndex = 2;
+            this.btnFormulario104.Text = "FORMULARIO 104";
+            this.btnFormulario104.UseVisualStyleBackColor = true;
+            // 
+            // MenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFormulario104);
+            this.Controls.Add(this.btnFormulario106);
+            this.Controls.Add(this.btnFormulario102A);
+            this.Name = "MenuPrincipal";
+            this.Text = "MenuPrincipal";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFormulario102A;
+        private System.Windows.Forms.Button btnFormulario106;
+        private System.Windows.Forms.Button btnFormulario104;
+    }
+}
